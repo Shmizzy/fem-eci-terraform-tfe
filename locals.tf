9d51780a8d6b32b91c570fc4c6face694f5cc239
izzylppz <https://github.com/Shmizzy>
2024-10-20 19:51:07 -0500

    feat: setup

--- locals.tf
+++ locals.tf
@@ -11,7 +11,16 @@ locals {
             description = "Example description of the workspace"
             execution_mode = "remote"
             project_id = module.project["fem-eci-project"].id
-            vcs_repo_identifier = "Shmizzy/fem-eci-terraform-tfe"
+            vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+        }
+
+        "fem-eci-github" = {
+            description = "Example automation workspace for GitHub resources."
+            execution_mode = "local"
+            project_id = module.project["fem-eci-project"].id
+            vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-eks"
         }
     }
+
+
 }
